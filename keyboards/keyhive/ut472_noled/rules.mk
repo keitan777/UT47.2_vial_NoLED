8935544513bc9de55f3fb7f547f68adcee23f195
keitan777 <keitan777illum777@docomo.ne.jp>
2026-08-12 11:41:02 +0900

    Optimize Stage 2 Vial firmware features

---
+++ keyboards/keyhive/ut472_noled/rules.mk
@@ -0,0 +1,22 @@
+# ATmega32U2 size optimization for Vial
+LTO_ENABLE = yes
+
+# Features required by the existing UT47.2 keymap
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPACE_CADET_ENABLE = yes
+
+# Features not used by the keymap / Vial configuration
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+BOOTMAGIC_ENABLE = no
+NKRO_ENABLE = no
+AUDIO_ENABLE = no
+MUSIC_ENABLE = no
+CAPS_WORD_ENABLE = no
+AUTO_SHIFT_ENABLE = no
+REPEAT_KEY_ENABLE = no
+MAGIC_ENABLE = no
+SWAP_HANDS_ENABLE = no
+
+# Keep Grave Escape, Tap Dance, Combo, Layer State and normal tapping behavior.
