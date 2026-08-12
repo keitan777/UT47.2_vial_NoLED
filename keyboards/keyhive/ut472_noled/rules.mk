fe654d81db9aa1fc6fcc2aa77ff8caffab766a89
keitan777 <keitan777illum777@docomo.ne.jp>
2026-08-12 12:58:42 +0900

    Remove LED features and enable Tap Dance/Combo

--- keyboards/keyhive/ut472_noled/rules.mk
+++ keyboards/keyhive/ut472_noled/rules.mk
@@ -6,6 +6,10 @@ MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 SPACE_CADET_ENABLE = yes
 
+# Vial dynamic features for the next test
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
 # Features not used by the keymap / Vial configuration
 COMMAND_ENABLE = no
 CONSOLE_ENABLE = no
@@ -18,5 +22,7 @@ AUTO_SHIFT_ENABLE = no
 REPEAT_KEY_ENABLE = no
 MAGIC_ENABLE = no
 SWAP_HANDS_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+QMK_SETTINGS = no
 
-# Keep Grave Escape, Tap Dance, Combo, Layer State and normal tapping behavior.
+# No RGB / WS2812 / LED functionality.
