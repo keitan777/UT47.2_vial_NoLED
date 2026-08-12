be93addca2532593ad8509b80e96246e15fc8f3a
keitan777 <keitan777illum777@docomo.ne.jp>
2026-08-12 12:44:40 +0900

    Restore standard Vial protocol feature set for HID communication

--- keyboards/keyhive/ut472_noled/keymaps/vial/rules.mk
+++ keyboards/keyhive/ut472_noled/keymaps/vial/rules.mk
@@ -1,27 +1,15 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
-# AVR size reduction. Keep features required by this build.
+# AVR size optimization
 LTO_ENABLE = yes
-QMK_SETTINGS = no
-NO_ACTION_ONESHOT = yes
-NO_ACTION_MACRO = yes
-NO_ACTION_FUNCTION = yes
-NO_MUSIC = yes
-NO_MUSIC_MAP = yes
-NO_VELOCIKEY = yes
-NO_COMBINED_KEYS = yes
-NO_ALT_TAB = yes
-NO_REPEAT_KEY = yes
-NO_CAPS_WORD = yes
-NO_AUTO_SHIFT = yes
-NO_MAGIC = yes
-NO_SWAP_HANDS = yes
-NO_OSM = yes
-NO_TRI_LAYER = yes
-NO_LOCKING_SUPPORT = yes
 
-# Vial features intentionally retained.
+# Features used by the UT47.2 keymap
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPACE_CADET_ENABLE = yes
+
+# Vial features required by this build
 TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
