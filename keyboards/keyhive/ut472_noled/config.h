9fef01f11b827fd753844847a747cc536d6f1de6
keitan777 <keitan777illum777@docomo.ne.jp>
2026-08-12 18:20:15 +0900

    Increase Vial Macro Tap Dance and Combo capacities

--- keyboards/keyhive/ut472_noled/config.h
+++ keyboards/keyhive/ut472_noled/config.h
@@ -29,8 +29,8 @@
 #define LAYER_STATE_8BIT
 
 /* Vial dynamic feature limits for this test build. */
-#define VIAL_TAP_DANCE_ENTRIES 8
-#define VIAL_COMBO_ENTRIES 12
-#define DYNAMIC_KEYMAP_MACRO_COUNT 6
+#define VIAL_TAP_DANCE_ENTRIES 16
+#define VIAL_COMBO_ENTRIES 16
+#define DYNAMIC_KEYMAP_MACRO_COUNT 8
 
 /* No RGB/WS2812/LED functionality. */
