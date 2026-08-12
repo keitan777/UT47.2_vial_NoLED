b645849bf112d0e314a2f96bf4c97621257b84f7
keitan777 <keitan777illum777@docomo.ne.jp>
2026-08-12 12:52:58 +0900

    Disable unused Vial dynamic features to reduce SRAM

--- keyboards/keyhive/ut472_noled/keymaps/vial/rules.mk
+++ keyboards/keyhive/ut472_noled/keymaps/vial/rules.mk
@@ -1,15 +1,28 @@
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-
-# AVR size optimization
+# ATmega32U2 size optimization for Vial
 LTO_ENABLE = yes
 
-# Features used by the UT47.2 keymap
+# Features required by the existing UT47.2 keymap
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 SPACE_CADET_ENABLE = yes
 
-# Vial features required by this build
-TAP_DANCE_ENABLE = yes
-COMBO_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
+# Vial dynamic features are not used by the existing keymap.
+# Disable them explicitly to avoid their runtime RAM overhead.
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+REPEAT_KEY_ENABLE = no
+
+# Features not used by the keymap / Vial configuration
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+BOOTMAGIC_ENABLE = no
+NKRO_ENABLE = no
+AUDIO_ENABLE = no
+MUSIC_ENABLE = no
+CAPS_WORD_ENABLE = no
+AUTO_SHIFT_ENABLE = no
+MAGIC_ENABLE = no
+SWAP_HANDS_ENABLE = no
+
+# Keep Grave Escape, Layer State and normal tapping behavior.
