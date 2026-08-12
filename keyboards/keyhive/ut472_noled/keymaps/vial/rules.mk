5b84fb738cb70ec766f288149058061f40b8ae88
keitan777 <keitan777illum777@docomo.ne.jp>
2026-08-12 16:39:25 +0900

    Restore VIA and Vial protocol for Tap Dance 4 Combo 8 test

--- keyboards/keyhive/ut472_noled/keymaps/vial/rules.mk
+++ keyboards/keyhive/ut472_noled/keymaps/vial/rules.mk
@@ -1,15 +1,18 @@
 # ATmega32U2 size optimization for Vial
 LTO_ENABLE = yes
 
+# Vial protocol
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
 # Features required by the existing UT47.2 keymap
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 SPACE_CADET_ENABLE = yes
 
-# Vial dynamic features are not used by the existing keymap.
-# Disable them explicitly to avoid their runtime RAM overhead.
-TAP_DANCE_ENABLE = no
-COMBO_ENABLE = no
+# Vial dynamic features for this test build
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
 KEY_OVERRIDE_ENABLE = no
 REPEAT_KEY_ENABLE = no
 
@@ -24,5 +27,6 @@ CAPS_WORD_ENABLE = no
 AUTO_SHIFT_ENABLE = no
 MAGIC_ENABLE = no
 SWAP_HANDS_ENABLE = no
+QMK_SETTINGS = no
 
-# Keep Grave Escape, Layer State and normal tapping behavior.
+# No RGB / WS2812 / LED functionality.
